9490531f431560bcc79c44395e855f35744c599d
Sam Johnson <sam@durosoft.com>
2019-01-26 01:07:33 -0500

    set up example function

--- spec/example_spec.cr
+++ spec/example_spec.cr
@@ -1,6 +1,6 @@
 require "./spec_helper"
 
-describe Cf::Kemal do
+describe ExampleFunction do
   # TODO: Write tests
 
   it "works" do
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/cf-kemal"
+require "../main.cr"
